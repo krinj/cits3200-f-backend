190210809a4107cca3e1610e9c0b4a3c7141c753
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-10-09 19:23:41 +0800

    Test Submit Response

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import random
 import time
 import uuid
+from typing import List
+from response_data import ResponseData
 
 from flask import Flask
 from google.cloud import bigquery
@@ -14,7 +16,12 @@ from google.cloud.language import types
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
+
+
 app = Flask(__name__)
+
+K_DATASET = "analytics"
+K_TABLE = "responses"
 
 
 @app.route('/')
@@ -29,6 +36,22 @@ def submit():
 
 @app.route('/test_submit')
 def test_submit():
+
+    response_data = ResponseData()
+    response_data.year_of_birth = 1986
+    response_data.organization = "Google"
+    response_data.question_name = "How are you today?"
+    response_data.question_id = "nlp_how_are_you"
+    response_data.gender = "male"
+    response_data.timestamp = time.time()
+    response_data.employment_status = "full_time"
+    response_data.response = "I had a great day today! I love this new CEO, he bought us a fantastic ping pong table."
+    response_data.survey_id = uuid.uuid4().hex
+    response_data.submission_id = uuid.uuid4().hex
+    response_data.survey_name = "default_survey"
+
+    _upload_response([response_data])
+
     return "Test Submission With Fake Data"
 
 
@@ -53,6 +76,24 @@ def test_submit():
 #     except Exception as e:
 #         return str(e)
 
+def _upload_response(response_data_list: List[ResponseData]):
+    """ Upload this response data to the BigQuery table. """
+
+    # Setup the client.
+    client = bigquery.Client()
+    table_ref = client.dataset(K_DATASET).table(K_TABLE)
+    table = client.get_table(table_ref)
+
+    # Convert the responses to JSON format and submit them.
+    rows_to_insert = [r.export_as_json() for r in response_data_list]
+    errors = client.insert_rows(table, rows_to_insert)
+
+    # Return the error status.
+    if not errors:
+        return "Submit Data Success"
+    else:
+        return "Submit Data Failure"
+
 
 def submit_nlp_request(text):
     # Instantiates a client
@@ -67,32 +108,6 @@ def submit_nlp_request(text):
     sentiment = client.analyze_sentiment(document=document).document_sentiment
     print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
     return sentiment.score, sentiment.magnitude
-
-
-def submit_data(text, s_score, s_mag):
-
-    client = bigquery.Client()
-    dataset_id = "test_db_ae"
-    table_id = "items"  # replace with your table ID
-    table_ref = client.dataset(dataset_id).table(table_id)
-    table = client.get_table(table_ref)  # API request
-
-    name = random.choice(["Jim", "Sarah", "Bob", "Alan", "Steve", "Mary", "Emily"])
-    organization = random.choice(["Curtin", "UWA", "ECU", "Murdoch", "Stanford", "MIT", "Harvard"])
-    age = random.randrange(18, 70)
-
-    time_str = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    id = uuid.uuid4().hex
-
-    rows_to_insert = [
-        (name, organization, age, text, "", time_str, id, s_score, s_mag)
-    ]
-
-    errors = client.insert_rows(table, rows_to_insert)  # API request
-    if not errors:
-        return "Submit Data Success"
-    else:
-        return "Submit Data Failure"
 
 
 if __name__ == '__main__':
---
+++ response_data.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+"""
+Contains a data structure to help format the rows for pushing
+into BigQuery.
+"""
+
+import time
+import uuid
+from typing import List
+
+__author__ = "Jakrin Juangbhanich"
+__email__ = "juangbhanich.k@gmail.com"
+
+
+# ===================================================================================================
+# Table Keys: Check that these names match the field names on big table.
+# ===================================================================================================
+
+K_KEY: str = "key"
+K_ORGANIZATION: str = "organization"
+K_GENDER: str = "gender"
+K_TIMESTAMP: str = "timestamp"
+K_YEAR_OF_BIRTH: str = "year_of_birth"
+K_EMPLOYMENT_STATUS: str = "employment_status"
+K_SUBMISSION_ID: str = "submission_id"
+K_SURVEY_ID: str = "survey_id"
+K_SURVEY_NAME: str = "survey_name"
+K_QUESTION_ID: str = "question_id"
+K_QUESTION_NAME: str = "question_name"
+K_RESPONSE: str = "response"
+K_OVERALL_SENTIMENT: str = "overall_sentiment"
+K_ENTITY: str = "entity"
+K_ENTITY_NAME: str = "name"
+K_ENTITY_SCORE: str = "score"
+
+# ===================================================================================================
+# Data Structure.
+# ===================================================================================================
+
+
+class Entity:
+    def __init__(self, name: str = "Entity Name", score: float = 0.5):
+        self.name: str = name
+        self.score: float = score
+
+    def data(self):
+        return {
+            K_ENTITY_NAME: self.name,
+            K_ENTITY_SCORE: self.score
+        }
+
+
+class ResponseData:
+    def __init__(self):
+        timestamp = int(time.time())
+        self.uid = uuid.uuid4().hex
+        self.organization: str = "organization"
+        self.key: str = f"{self.organization}_{timestamp}_{self.uid}"
+        self.gender: str = "male"
+        self.timestamp: int = timestamp
+        self.year_of_birth: int = 1990
+        self.employment_status: str = "full_time"
+        self.submission_id: str = self.uid
+        self.survey_id: str = self.uid
+        self.survey_name: str = "Survey Name"
+        self.question_id: str = "QuestionID"
+        self.question_name: str = "Question Name"
+        self.response: str = "Response"
+        self.overall_sentiment: float = 0.5
+        self.entity: List[Entity] = []
+
+    def generate_key(self):
+        self.key = f"{self.organization.lower()}_{self.timestamp}_{self.uid}"
+
+    def add_entity(self, name: str, score: float):
+        self.entity.append(Entity(name, score))
+
+    @property
+    def _entity_as_data(self):
+        """ Get all the entities in this object as a JSON object. """
+        return [e.data() for e in self.entity]
+
+    def export_as_json(self):
+        """ Return this entire response as a JSON data object. """
+        self.generate_key()
+        data = {
+            K_KEY: self.key,
+            K_ORGANIZATION: self.organization,
+            K_GENDER: self.gender,
+            K_TIMESTAMP: self.timestamp,
+            K_YEAR_OF_BIRTH: self.year_of_birth,
+            K_EMPLOYMENT_STATUS: self.employment_status,
+            K_SUBMISSION_ID: self.submission_id,
+            K_SURVEY_ID: self.survey_id,
+            K_SURVEY_NAME: self.survey_name,
+            K_QUESTION_ID: self.question_id,
+            K_QUESTION_NAME: self.question_name,
+            K_RESPONSE: self.response,
+            K_OVERALL_SENTIMENT: self.overall_sentiment,
+            K_ENTITY: self._entity_as_data
+        }
+        return data
